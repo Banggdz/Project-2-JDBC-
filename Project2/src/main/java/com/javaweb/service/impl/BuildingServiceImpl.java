bc1fd8dc49e1a7fa71f1a3767a6a3b9feb8b8577
Banggdz <nguyenhuubangzero@gmail.com>
2025-09-05 00:09:19 +0700

    Update BuildingServiceImpl.java

--- Project2/src/main/java/com/javaweb/service/impl/BuildingServiceImpl.java
+++ Project2/src/main/java/com/javaweb/service/impl/BuildingServiceImpl.java
@@ -29,6 +29,27 @@ public class BuildingServiceImpl implements BuildingService{
 			BuildingDTO building = buildingDTOConverter.toBuildingDTO(item);
 			result.add(building);
 		}
+		// BuildingSearchBuilder buildingSearchBuilder = buildingSearchBuilderConverter.toBuildingSearchBuilder(params, typeCode);
+		// List<BuildingEntity> buildingEntities = buildingRepository.findAll(buildingSearchBuilder);
+		
+		// List<BuildingDTO> result = new ArrayList<>();
+		// for(BuildingEntity item : buildingEntities) {
+		// 	BuildingDTO building = new BuildingDTO();
+
+		// 	building.setName(item.getName());
+		// 	building.setManagerName(item.getManagerName());
+		// 	building.setManagerPhoneNumber(item.getManagerPhoneNumber());
+
+		// 	building.setFloorArea(item.getFloorArea());
+		// 	building.setRentArea(item.getRentArea());
+		// 	building.setEmptyArea(item.getEmptyArea());
+
+		// 	building.setRentPrice(item.getRentPrice());
+		// 	building.setServiceFee(item.getServiceFee());
+		// 	building.setBrokerageFee(item.getBrokerageFee());
+
+		// 	result.add(building);
+		// }
 		return result;
 	}
 }
