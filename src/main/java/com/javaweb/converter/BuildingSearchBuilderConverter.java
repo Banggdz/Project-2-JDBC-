5d5f7d5a999c4a13e5d360c783348cf4fae3b7c2
Banggdz <nguyenhuubangzero@gmail.com>
2025-09-05 09:33:04 +0700

    Update BuildingSearchBuilderConverter.java

--- src/main/java/com/javaweb/converter/BuildingSearchBuilderConverter.java
+++ src/main/java/com/javaweb/converter/BuildingSearchBuilderConverter.java
@@ -31,4 +31,23 @@ public class BuildingSearchBuilderConverter {
 				.build();
 		return buildingSearchBuilder;
 	}
+	// public BuildingSearchBuilder toBuildingSearchBuilder(Map<String,Object> params,List<String> typeCode) {
+	// 	BuildingSearchBuilder buildingSearchBuilder = new BuildingSearchBuilder.Builder()
+	// 			.setName((String) params.get("name"))
+	//             .setFloorArea(params.get("floorArea") != null ? Long.parseLong(params.get("floorArea").toString()) : null)
+	//             .setWard((String) params.get("ward"))
+	//             .setStreet((String) params.get("street"))
+	//             .setDistrictCode((String) params.get("districtId"))
+	//             .setNumberOfBasement(params.get("numberOfBasement") != null ? Integer.parseInt(params.get("numberOfBasement").toString()) : null)
+	//             .setManagerName((String) params.get("managerName"))
+	//             .setManagerPhoneNumber((String) params.get("managerPhoneNumber"))
+	//             .setRentPriceTo(params.get("rentPriceTo") != null ? Long.parseLong(params.get("rentPriceTo").toString()) : null)
+	//             .setRentPriceFrom(params.get("rentPriceFrom") != null ? Long.parseLong(params.get("rentPriceFrom").toString()) : null)
+	//             .setAreaFrom(params.get("areaFrom") != null ? Long.parseLong(params.get("areaFrom").toString()) : null)
+	//             .setAreaTo(params.get("areaTo") != null ? Long.parseLong(params.get("areaTo").toString()) : null)
+	//             .setStaffId(params.get("staffId") != null ? Long.parseLong(params.get("staffId").toString()) : null)
+	//             .setTypeCode(typeCode)
+	//             .build();
+	// 	return buildingSearchBuilder;
+	// }
 }
